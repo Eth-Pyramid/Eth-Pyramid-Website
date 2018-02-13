5d373b9c95250ae6b05b70a4b638873e7e11c10f
tocsick <tocsick_ethpy@protonmail.com>
2018-02-13 22:59:24 +1000

    Actuall fix router

--- router.php
+++ router.php
@@ -6,7 +6,15 @@ $uri = substr(strtok($_SERVER['REQUEST_URI'], '?'), 1);
 if (file_exists(__DIR__ . '/' . $uri))
 	return false;
 
-if (substr($uri, -5) == '.html')
-	include substr($uri, 0, -5) . '.php';
+if (substr($uri, -5) == '.html'){
+	$php_uri = substr($uri, 0, -5) . '.php';
+
+	if (file_exists(__DIR__ . '/' . $php_uri)){
+		include $php_uri;
+		return true;
+	}
+}
 
 echo 'File not found';
+
+
