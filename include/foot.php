762bbbc3fe96980eeb7a2b6276465d1601a7dfb3
tocsick <tocsick_ethpy@protonmail.com>
2018-02-14 22:18:18 +1000

    Removed new lines from translation

--- include/foot.php
+++ include/foot.php
@@ -23,11 +23,7 @@
             <div class="ten wide column">
                 <h4 class="ui inverted header"><?php __('Disclaimer'); ?></h4>
                 <p>
-                    <?php __('This game is intended for entertainment purposes only. Do not send any ETH you do not want to or
-                    cannot afford to lose. There is zero guarantee that you will make ETH playing this game. And more
-                    importantly, zero guarantee that you won\'t lose any ETH you play with. Play at your own risk and do
-                    not attempt to pressure friends, family or strangers into playing too. In fact, you shouldn\'t even
-                    play yourself.'); ?>
+                    <?php __('This game is intended for entertainment purposes only. Do not send any ETH you do not want to or cannot afford to lose. There is zero guarantee that you will make ETH playing this game. And more importantly, zero guarantee that you won\'t lose any ETH you play with. Play at your own risk and do not attempt to pressure friends, family or strangers into playing too. In fact, you shouldn\'t even play yourself.'); ?>
                 </p>
             </div>
         </div>
