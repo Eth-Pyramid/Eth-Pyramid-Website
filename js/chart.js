8ce2f34a5e0a8ead35f03a4cbb851bb4f729ea8d
tocsick <tocsick_ethpy@protonmail.com>
2018-02-15 19:17:06 +1000

    Tweak chart

--- js/chart.js
+++ js/chart.js
@@ -1,109 +1,113 @@
-var chart = AmCharts.makeChart( "chartdiv", {
-  "type": "stock",
-  "theme": "light",
+var chart = AmCharts.makeChart('chartdiv', {
+  'type': 'stock',
+  'theme': 'light',
 
-  "categoryAxesSettings": {
-    "minPeriod": "15mm"
+  'categoryAxesSettings': {
+    'minPeriod': '15mm',
+    'maxSeries': 150,
+    'groupToPeriods': ['15mm', '30mm', 'hh', '2hh', '3hh', '6hh', '12hh', 'DD', '2DD', '3DD', '4DD', '5DD', '6DD', 'WW', '2WW', 'MM', '3MM', '6MM', 'YYYYt st']
   },
 
-  "dataSets": [ {
-    "fieldMappings": [ {
-      "fromField": "open",
-      "toField": "open"
+  'dataSets': [{
+    'fieldMappings': [{
+      'fromField': 'open',
+      'toField': 'open'
     }, {
-      "fromField": "close",
-      "toField": "close"
+      'fromField': 'close',
+      'toField': 'close'
     }, {
-      "fromField": "high",
-      "toField": "high"
+      'fromField': 'high',
+      'toField': 'high'
     }, {
-      "fromField": "low",
-      "toField": "low"
-    } ],
-    "dataProvider": [],
-    "color": "#7f8da9",
-    "title": "EPY Sell Price",
-    "categoryField": "date"
-  } ],
+      'fromField': 'low',
+      'toField': 'low'
+    }],
+    'dataProvider': [],
+    'color': '#7f8da9',
+    'title': 'EPY Sell Price',
+    'categoryField': 'date'
+  }],
 
-  "panels": [ {
-    "title": "Value",
+  'panels': [{
+    'title': 'Value',
 
-    "showCategoryAxis": true,
+    'showCategoryAxis': true,
 
-    "percentHeight": 70,
+    'percentHeight': 70,
 
-    "dataDateFormat":"YYYY-MM-DD JJ:NN",
+    'dataDateFormat': 'YYYY-MM-DD JJ:NN',
 
-    "valueAxes": [ {
-      "dashLength": 5
-    } ],
+    'valueAxes': [{
+      'dashLength': 5
+    }],
 
-    "categoryField": "date",
+    'categoryField': 'date',
 
-    "categoryAxis": {
-      "parseDates": true
+    'categoryAxis': {
+      'parseDates': true
     },
 
-    "chartCursor": {
-      "valueLineEnabled": true,
-      "valueLineBalloonEnabled": true
+    'chartCursor': {
+      'valueLineEnabled': true,
+      'valueLineBalloonEnabled': true
     },
 
-    "stockGraphs": [ {
-      "type": "candlestick",
-      "id": "g1",
-      "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
-      "openField": "open",
-      "closeField": "close",
-      "highField": "high",
-      "lowField": "low",
-      "valueField": "close",
-      "lineColor": "#7f8da9",
-      "fillColors": "#7f8da9",
-      "negativeLineColor": "#db4c3c",
-      "negativeFillColors": "#db4c3c",
-      "fillAlphas": 1,
-      "useDataSetColors": false,
-      "comparable": true,
-      "compareField": "value",
-      "showBalloon": true
-    } ]
-  } ],
+    'stockGraphs': [{
+      'type': 'candlestick',
+      'id': 'g1',
+      'balloonText': 'Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>',
+      'openField': 'open',
+      'closeField': 'close',
+      'highField': 'high',
+      'lowField': 'low',
+      'valueField': 'close',
+      'lineColor': '#5ba976',
+      'fillColors': '#5ba976',
+      'negativeLineColor': '#db4c3c',
+      'negativeFillColors': '#db4c3c',
+      'fillAlphas': 1,
+      'useDataSetColors': false,
+      'comparable': true,
+      'compareField': 'value',
+      'showBalloon': true
+    }]
+  }],
 
-  "chartScrollbarSettings": {
-    "graph": "g1",
-    "graphType": "line",
-    "usePeriod": "15mm"
+  'chartScrollbarSettings': {
+    'graph': 'g1',
+    'graphType': 'line',
+    'usePeriod': '15mm',
+    'height': 90,
+    'color': '#222222'
   }
 
-} );
+})
 
-chart.addListener( "dataUpdated", zoomChart );
+chart.addListener('dataUpdated', zoomChart)
 
-var zoomed = false;
+var zoomed = false
 
-function zoomChart() {
-  var diff = Math.min( 96*5, chart.dataSets[0].dataProvider.length );
-  if( chart.dataSets[0].dataProvider.length === 0 )
-    return;
+function zoomChart () {
+  var diff = Math.min(96 * 5, chart.dataSets[0].dataProvider.length)
+  if (chart.dataSets[0].dataProvider.length === 0)
+    return
 
-  if( zoomed )
+  if (zoomed)
     return
 
   zoomed = true
 
-  var start = new Date(chart.dataSets[0].dataProvider[ chart.dataSets[0].dataProvider.length-diff-1 ].date + ":00");
-  var end = new Date(chart.dataSets[0].dataProvider[ chart.dataSets[0].dataProvider.length-1 ].date + ":00");
-  chart.zoom( start, end );
+  var start = new Date(chart.dataSets[0].dataProvider[chart.dataSets[0].dataProvider.length - diff - 1].date + ':00')
+  var end = new Date(chart.dataSets[0].dataProvider[chart.dataSets[0].dataProvider.length - 1].date + ':00')
+  chart.zoom(start, end)
 }
 
-function refreshData(){
-  $.getJSON( 'https://api.ethpyramid.io/candles.php', function( data ){
+function refreshData () {
+  $.getJSON('https://api.ethpyramid.io/candles.php', function (data) {
     chart.dataSets[0].dataProvider = data
-    chart.validateData();
+    chart.validateData()
   })
 }
 
 refreshData()
-setInterval( refreshData, 1000 * 60 )
+setInterval(refreshData, 1000 * 60)
