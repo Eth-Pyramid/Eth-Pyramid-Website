e9b7800f222639b1981a181aea47771ddc3bb74d
tocsick <tocsick_ethpy@protonmail.com>
2018-02-15 20:49:25 +1000

    Fix donut chart colors

--- stats.php
+++ stats.php
@@ -38,7 +38,7 @@ include('include/head.php');
                 </div>
                 <div class="ui statistic column">
                     <div class="label">
-			            <?php __('Total Dividends'); ?>
+						<?php __('Total Dividends'); ?>
                     </div>
                     <div class="value" id="total-dividends">
                         0 ETHt s
@@ -61,10 +61,45 @@ include('include/head.php');
     </div>
 
     <script type="text/javascript">
+      /**
+       * Converts an HSL color value to RGB. Conversion formula
+       * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+       * Assumes h, s, and l are contained in the set [0, 1] and
+       * returns r, g, and b in the set [0, 255].
+       *
+       * @param   {number}  h       The hue
+       * @param   {number}  s       The saturation
+       * @param   {number}  l       The lightness
+       * @return  {string}           The RGB representation
+       */
+      function hslToRgb (h, s, l) {
+        var r, g, b
+
+        if (s == 0) {
+          r = g = b = l // achromatic
+        } else {
+          var hue2rgb = function hue2rgb (p, q, t) {
+            if (t < 0) t += 1
+            if (t > 1) t -= 1
+            if (t < 1 / 6) return p + (q - p) * 6 * t
+            if (t < 1 / 2) return q
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+            return p
+          }
+
+          var q = l < 0.5 ? l * (1 + s) : l + s - l * s
+          var p = 2 * l - q
+          r = hue2rgb(p, q, h + 1 / 3)
+          g = hue2rgb(p, q, h)
+          b = hue2rgb(p, q, h - 1 / 3)
+        }
+
+        return '#' + Math.round(r * 255).toString(16) + Math.round(g * 255).toString(16) + Math.round(b * 255).toString(16)
+      }
+
       function selectColor (colorNum, colors) {
         colorNum = (colorNum * colorNum) % colors
-        if (colors < 1) colors = 1
-        return 'hsl(' + (colorNum * (360 / colors) % 360) + ',60%,50%)'
+        return hslToRgb(1 / colors * colorNum, 0.6, 0.5)
       }
 
       var background_colors = []
@@ -108,10 +143,10 @@ include('include/head.php');
       })
 
       $.getJSON('https://api.ethpyramid.io/stats.php', function (data) {
-        $( '#epy-supply' ).html( data.total_supply.toFixed(0) + ' EPY' );
-        $( '#total-volume' ).html( data.total_volume.toFixed(0) + ' ETH' );
-        $( '#contract-balance' ).html( data.contract_balance.toFixed(0) + ' ETH' );
-        $( '#total-dividends' ).html( (data.total_volume/10).toFixed(0) + ' ETH' );
+        $('#epy-supply').html(data.total_supply.toFixed(0) + ' EPY')
+        $('#total-volume').html(data.total_volume.toFixed(0) + ' ETH')
+        $('#contract-balance').html(data.contract_balance.toFixed(0) + ' ETH')
+        $('#total-dividends').html((data.total_volume / 10).toFixed(0) + ' ETH')
       })
     </script>
 
