e5b237378319f435c9e3d3aaba35bc4eee8eabd3
Dustin Frank <dustin.frank@web.de>
2018-02-17 02:51:42 +0100

    Fix other langs

--- lang/au.php
+++ lang/au.php
@@ -7,8 +7,8 @@ return [
 	'default_currency' => 'AUD',
 	'flag' => 'AU.jpg',
 	'text' => [
-		'My Coins' => 'My Mullah',
-		'A <b>self-sustaining</b>, <b>secure</b> and <b>transparent</b> pyramid scheme.' => 'A <b>self-sustaining</b>, <b>secure</b> and <b>transparent</b> pyramid scheme, mates.',
-		'Purchase EthPyramid Tokens' => 'Purchase EthPyramid Tokens, mate',
+		'menu.coins' => 'My Mullah',
+		'index.header.title' => 'A <b>self-sustaining</b>, <b>secure</b> and <b>transparent</b> pyramid scheme, mates.',
+		'coins.heading' => 'Purchase EthPyramid Tokens, mate',
 	]
 ];
