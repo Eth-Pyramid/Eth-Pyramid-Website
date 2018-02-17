08b551a2c9ba749020fc25c2cd6447eea068a8ba
Anonymous <vitalik_madoff@protonmail.com>
2018-02-18 03:37:51 +0800

    Added snek (heh) peek of Hindi

--- include/head-home.php
+++ include/head-home.php
@@ -129,6 +129,7 @@
                             <a href="?lang=cn" class="item text"><i class="cn flag"></i> Chinese</a>
                             <a href="?lang=fr" class="item text"><i class="fr flag"></i> French</a>
                             <a href="?lang=de" class="item text"><i class="de flag"></i> German</a>
+                            <a href="?lang=us" class="item text"><i class="in flag"></i> Hindi [soon!]</a>
                             <a href="?lang=kr" class="item text"><i class="kr flag"></i> Korean</a>
                             <a href="?lang=es" class="item text"><i class="es flag"></i> Spanish</a>
                             <a href="?lang=ru" class="item text"><i class="ru flag"></i> Russian</a>
