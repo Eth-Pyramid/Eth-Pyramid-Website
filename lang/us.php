49a2dcc11713f8e9cb193d41d83e8c492b8710ce
Dustin Frank <dustin.frank@web.de>
2018-02-17 15:11:33 +0100

    Update and format some langs

--- lang/us.php
+++ lang/us.php
@@ -76,6 +76,11 @@ return [
         'coins.network-notice' => 'Depending on the Ethereum network traffic, figures may be delayed.',
         'coins.contract-balance' => 'Contract Balance:',
 
+        'coins.history.log.sent-eth' => 'Bought {1} EPY for {0} ETH',
+        'coins.history.log.sent-div' => 'Bought {0} EPY using dividends',
+        'coins.history.log.withdrew' => 'Withdrew dividends',
+        'coins.history.log.sold-epy' => 'Sold {0} EPY'
+
         'coins.actions.cash-out.button' => 'Sell Tokens',
         'coins.actions.cash-out.comment' => 'Your tokens will be sold at the sell-price and will be converted into Ether dividends.',
         'coins.actions.pay-divs.button' => 'Buy With Dividends',
@@ -89,7 +94,6 @@ return [
 	    'donate.heading' => 'Donate to the EthPyramid Marketing Fund',
 	    'donate.description' => 'The donation address is a multi-sig wallet that requires two core developers to approve any withdrawals. Funds will be used solely for the purpose of advertising EthPyramid.',
 	    'donate.address' => 'If you prefer, you can send donations directly to <a href="https://etherscan.io/address/0x25dd53e2594735b38a4646f62e5b65b4e4aa42bb" target="_blank">0x25dd53e2594735b38a4646f62e5b65b4e4aa42bb</a>',
-
 
         'chart.heading' => 'Sell Price Chart',
 
@@ -108,9 +112,8 @@ return [
         'stats.total-volume' => 'Total Volume',
         'stats.eth-balance' => 'Contract Balance',
         'stats.total-divs' => 'Total Dividends',
-
         'stats.top25.heading' => 'Top 25 Token Holders',
-        'stats.top25.comment' => 'Top 25 Token Holders',
+        'stats.top25.comment' => 'Aliases of dev team members are shown in place of address if known.',
         'stats.daily.heading' => 'Daily Volume (Last 7 days)',
         'stats.daily.comment' => 'Volume is in ETH and timezone is UTC.',
 
