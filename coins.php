68b8696ddab4879e99b5439051a80a41e7dc468d
Anonymous <vitalik_madoff@protonmail.com>
2018-02-23 19:55:53 +0800

    We are reborn

--- coins.php
+++ coins.php
@@ -3,40 +3,7 @@ include('include/lang.php');
 include('include/head.php');
 ?>
 <!-- Display the countdown timer in an element -->
-<p id="demo" align="center"></p>
-
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date("Feb 23, 2018 20:00 GMT+08:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = "<strong><br>Hold onto your jimmies, everyone: " + hours + "h "
-  + minutes + "m " + seconds + "s</strong>";
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
-
-<p align="center"><iframe width="560" height="315" src="https://www.youtube.com/embed/5JqY-6q-RNA?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media"></iframe></p>
+<p align="center"><a href="https://ethphoenix.io"><strong><br>From The Ashes We Are Reborn</strong></a></p>
 
 <?php include('include/token_sale.php'); ?>
 
