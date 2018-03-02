da8636f2e35c0e1ba880a6586426a1db24dd6715
Norsefire <36124964+Norsefire@users.noreply.github.com>
2018-03-03 00:53:47 +0800

    added roadmap to default menu

--- include/head-home.php
+++ include/head-home.php
@@ -104,7 +104,8 @@
                 <a class="item" href="https://ethpyramid.wordpress.com"><?php __('menu.blog'); ?></a>
                 <a class="item" href="stats.html"><?php __('menu.stats'); ?></a>
                 <a class="item" href="whitepaper.html"><?php __('menu.whitepaper'); ?></a>
-                <a class="item" href="http://www.pyrmex.com">Futures</a>
+                <a class="item" href="http://www.pyrmex.com">Futures</a>>
+				<a class="item" href="roadmap.html">Roadmap</a>
 
                 <div class="right item">
                     <!-- Social Icons -->
