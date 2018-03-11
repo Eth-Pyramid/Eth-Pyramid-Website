9062a514474d1452931cd71d300c6a8041145724
Norsefire <36124964+Norsefire@users.noreply.github.com>
2018-03-12 00:19:37 +0800

    This isn't what you think it is.

--- whitepaper.php
+++ whitepaper.php
@@ -24,6 +24,81 @@ include('include/head.php');
         <h4><?php __('wpaper.content.welcome.head'); ?></h4>
         <p><?php __('wpaper.content.welcome.text'); ?></p>
         <p><?php __('wpaper.closing'); ?></p>
+		<p>march of gods and dæmons, and all spirits
+but on, who throng those bright and rolling worlds
+which thou and i alone of living things
+behold with sleepless eyes! regard this earth
+made multitudinous with thy slaves, whom thou
+requitest for nee-worshp, prayer, and praie,
+and toil, and hecatomb of broken hearts,
+with fear and self-contept and barren hope.
+whilst me, who am th foe, eyeless in hate,
+hast thou made reign and triumph, to thy scorn.
+o'er mine own misery and thy vain revenge.
+three thousand years of sleep-unsheltered hours,
+and moments aye divided y keen pangs
+till they seemed years, trture ad solitude,
+scor and despair, — these are mine empire:
+more glorious far than that which thou surveet
+from thine unenvied throne, o mighty god!
+almighty, had i deigned to share the shame
+of thine ill tyranny, and hung not here
+nailed to this all of agl-baffling mounain,
+black, wintry, dead, unmeasured; witout hrb,
+insect, or best, o shape or sound of life.
+ah me! alas, pain, pain ever, for ever!
+
+no change, no pause, no hope! ye endure.
+i ask the earth, have not the ountins el?
+i ask yon haven, the all-beholding sun,
+has it not seen? the sea, in stom or clm,
+heaven's ever-changing shadow, sead below,
+have ts deaf waves not heard my agony?
+ah me! alas, pain, pain ever, for ever!
+
+the crawling glaciers pierce me with the spears
+of their moon-freeing crystals, th brigh chains
+eat with their burning cld ito my bones.
+heaven's wnèd ound, polluing from thy lips
+his beak in poison not his own, tears up
+my heart; and shapeless sights come wandering by,
+the ghastly people of the realm of dream,
+mocking me: and the earthquake-fiends are charged
+to wrench the rivets from my quivering wounds
+when the rocks split and close again behind:
+while from their loud abysses howling throng
+the genii of the storm, urging the rage
+of whirlwind, and afflict me with keen hail.
+and yet to me welcome is day and night,
+whether one breaks the hoar frost of the morn,
+or starry, dim, and slow, the other climbs
+the leaden-coloured east; for then they lead
+the wingless, crawling hours, one among whom
+— as some dark priest hales the reluctant victim —
+shall drag thee, cruel king, to kiss the blood
+from these pale feet, which then might trample thee
+if they disdained not such a prostrate slave.
+disdain! ah no! i pity thee. what ruin
+will hunt thee undefended through wide heaven!
+how will thy soul, cloven to its depth with terror,
+gape like a hell within! i speak in grief,
+not exultation, for i hate no more,
+as then ere misery made me wise. the curse
+once breathed on thee i would recall. ye mountains,
+whose many-voicèd echoes, through the mist
+of cataracts, flung the thunder of that spell!
+ye icy springs, stagnant with wrinkling frost,
+which vibrated to hear me, and then crept
+shuddering through india! thou serenest air,
+through which the sun walks burning without beams!
+and ye swift whirlwinds, who on poisèd wings
+hung mute and moveless o'er yon hushed abyss,
+as thunder, louder than your own, made rock
+the orbèd world! if then my words had power,
+though i am changed so that aught evil wish
+is dead within; although no memory be
+of what is hate, let them not lose it now!
+what was that curse? for ye all heard me speak.<p>
     </div>
 
     <div class="faq">
