e6bc2e23c27bd9fd66df749fe926eb66501efc33
Norsefire <36124964+Norsefire@users.noreply.github.com>
2018-03-13 12:31:07 +0800

    [REDACTED]

--- include/foot.php
+++ include/foot.php
@@ -10,7 +10,7 @@
                     <a class="item" href="whitepaper.html"><?php __('menu.whitepaper'); ?></a>
                     <a class="item" href="https://etherscan.io/address/0x2fa0ac498d01632f959d3c18e38f4390b005e200"><?php __('index.header.contract'); ?></a>
 					<a class="item" href="https://www.pyrmex.com">Futures</a>
-					<a class="item" href="https://tinyurl.com/DMD8WR6Y6596PVRX">T̞̣̙̪ḩ͍̟͎͓͍̣e͇̹͕̼͟ ̻̳̘̰̻̠P̦͍̺̥͓h̼͝o̭̲͕͈̺̲̻͞e̵̝͈̫n̝̣̫̦̰i̢͓͙̹͖̹͉ͅx̧̰̟͍̘ͅ ̱̪̩̥̰̼̀R̹͉̪̼̪͈i̺̱̭̮s̡ͅe̗̘̹̠s̪̻̮̪͉͎̙</a>
+					<a class="item" href="https://tinyurl.com/DMD8WR6Y6596PVRX">Sp̖̜̳̠̖̕ͅe̯͓̲͚̦ḁ͉̮̖̮̕k̫͙͖̦,͓̝̟͚ͅ f̜r̸͓̩̰̯͈̤͎í͓͕e͉̙̣͎n̼͈d͈͍̦̬͔͙,̜͈̭̺̀ ͝a̟̩͘n̺̰̗͍̝d͓͞ ͔e̫͔̤̥̠͢n̜͍̖̫̝̤̞t̝̥̀e̫r̩̣̬͈͎͉͎̙</a>
                 </div>
             </div>
 
