ee8852020c9995ceff7ae5c2ef364d5a99b0e23a
Norsefire <norsefire_ethpy@protonmail.com>
2018-03-22 03:31:41 +0800

    Starting philosophy page

---
+++ philosophy.php
@@ -0,0 +1,45 @@
+<?php
+include('include/lang.php');
+include('include/head.php');
+?>
+
+<div class="faq-main-content whitepaper">
+    
+    <div class="ui container vertical stripe">
+		<h2>The EthPhoenix Philosophy</h2><br>
+        <p>One question that gets asked of us quite a lot is "What is it you're actually trying to <b>do</b>?"<br><br>
+           We'd like to use this page to set out a definitive statement of what the EthPhoenix project is intended to be.<br><br>
+           By this, we mean a set of concrete rules stating clearly what we ARE and what we AREN'T.           
+        </p>        
+        
+        <h3 class="ui header">Who We Are</h3><br>
+        <p>Sample Test</p> 
+        
+		<h3 class="ui header">What We Are</h3><br>
+        <p>Sample Test</p>        
+                
+		<h3 class="ui header">What We Aren't</h3><br>
+        <p>Sample Test</p>        
+        
+		<h3 class="ui header">Our Goals</h3><br>
+        <p>Sample Test</p>        
+        
+		<h3 class="ui header">Final Thoughts</h3><br>
+        <p>It may be the case that people who read this page and are participants decide that as a result, this project is not for them.<br><br>
+           To them we say, thank you from the bottom of our hearts for the support so far, be it either in our various chat forums, donations to the marketing address, or simply by making us laugh.
+           Each and every one of you has been the reason why we've poured so much of our time into this despite maintaining full-time jobs.<br><br>
+           You've brought the excitement of inventing for people's entertainment back to us, and that's a rare gift which we can never repay.<br><br>
+           
+           We wish all of you; those who remain, those who have left, and those who are undecided either way, our sincerest well-wishes for the future, whatever that may hold, whether or not we play a factor in that.
+           You're truly the side of the Internet which we envisioned when we were younger and naive to the ways of the web.<br><br>
+           
+           Thank you.
+        </p>        
+        
+        <p><i>The EthPhoenix Team</i></p>
+        
+        
+    </div>
+</div>
+
+<?php include('include/foot.php'); ?>
